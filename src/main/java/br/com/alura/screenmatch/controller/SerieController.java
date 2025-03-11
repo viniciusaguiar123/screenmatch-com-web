0680fae9236508b290535ecbf850352af1e98307
Vinicius Aguiar <vini1068@gmail.com>
2025-03-10 21:09:24 -0300

    feat: add spring-boot-devtools dependency and implement series listing in SerieController

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,27 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String obterSeries(){
-        return "Aqui vão ser listadas as séries";
+    public List<SerieDTO> obterSeries(){
+        return repository.findAll()
+                .stream()
+                .map(serie -> new SerieDTO(serie.getId(), serie.getTitulo(), serie.getTotalTemporadas(),
+                        serie.getAvaliacao(), serie.getGenero(), serie.getAtores(), serie.getPosters(),
+                        serie.getSinopse())).collect(Collectors.toList());
     }
 }
